c3e63151020beefa3bba4f423cf8ef4970ffb44b
yeekit-jewel2 <yeekitjewel@gmail.com>
2024-09-13 18:52:23 +0800

    fix pre-commit

--- src/applications/tests/test_application.py
+++ src/applications/tests/test_application.py
@@ -1,26 +1,28 @@
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APIClient, APITestCase
-from ..models import Application
-from src.applicants.models import Applicant
-from src.users.models import User
-from src.schemes.models import Scheme, SchemeBenefit, SchemeCriteria
 from django.utils import timezone
 
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+
+from src.applicants.models import Applicant
+from src.schemes.models import Scheme, SchemeBenefit, SchemeCriteria
+from src.users.models import User
+from ..models import Application
+
+
 class ApplicationViewSetTestCase(APITestCase):
-    fixtures = ['dev.yaml']
+    fixtures = ["dev.yaml"]
 
     def setUp(self):
         # This method runs before every test
         self.client = APIClient()
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer 123')
-        self.base_url = '/v1/api/applications'
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer 123")
+        self.base_url = "/v1/api/applications"
         self.applicant = Applicant.objects.create(
             name="kelly ",
-            marital_status= "married",
+            marital_status="married",
             sex="male",
             employment_status="unemployed",
-            date_of_birth="1990-01-01"
+            date_of_birth="1990-01-01",
         )
         self.scheme = Scheme.objects.create(
             name="Give all unemployed with household kids 1000",
@@ -29,40 +31,42 @@ class ApplicationViewSetTestCase(APITestCase):
             name="School free meals credit",
             amount=1000,
             apply_household=True,
-            scheme_id=self.scheme
+            scheme_id=self.scheme,
         )
         SchemeCriteria.objects.create(
             name="All unemployed",
             ops="=",
             threshold="unemployed",
             field="employment_status",
-            scheme_id=self.scheme
+            scheme_id=self.scheme,
         )
         self.user = User.objects.get(username="admin")
         self.application = Application.objects.create(
             applicant=self.applicant,
             scheme=self.scheme,
-            reviewer = self.user,
-            created_time=timezone.now()
+            reviewer=self.user,
+            created_time=timezone.now(),
         )
 
     def test_create_application(self):
         data = {
             "applicant_uid": self.applicant.uid,
             "scheme_uid": self.scheme.uid,
-            "reviewer": self.user.uid
+            "reviewer": self.user.uid,
         }
-        response = self.client.post(self.base_url, data, follow=True, format='json')
+        response = self.client.post(self.base_url, data, follow=True, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_application(self):
         response = self.client.get(self.base_url, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['applicant']["uid"], str(self.applicant.uid))
+        self.assertEqual(response.data[0]["applicant"]["uid"], str(self.applicant.uid))
 
     def test_partial_update_application(self):
         data = {"status": "approved"}
-        response = self.client.patch(f'{self.base_url}/{self.application.uid}', data, format='json')
+        response = self.client.patch(
+            f"{self.base_url}/{self.application.uid}", data, format="json"
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['status'], data["status"])
+        self.assertEqual(response.data["status"], data["status"])
--- src/schemes/tests/test_scheme.py
+++ src/schemes/tests/test_scheme.py
@@ -1,17 +1,17 @@
-from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+
 from ..models import Scheme, SchemeBenefit, SchemeCriteria
 
 
 class SchemeViewSetTestCase(APITestCase):
-    fixtures = ['dev.yaml']
+    fixtures = ["dev.yaml"]
 
     def setUp(self):
         # This method runs before every test
         self.client = APIClient()
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer 123')
-        self.base_url = '/v1/api/schemes'
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer 123")
+        self.base_url = "/v1/api/schemes"
         self.scheme = Scheme.objects.create(
             name="Give all unemployed with household kids 1000",
         )
@@ -19,51 +19,59 @@ class SchemeViewSetTestCase(APITestCase):
             name="School free meals credit",
             amount=1000,
             apply_household=True,
-            scheme_id=self.scheme
+            scheme_id=self.scheme,
         )
         SchemeCriteria.objects.create(
             name="All unemployed",
             ops="=",
             threshold="unemployed",
             field="employment_status",
-            scheme_id=self.scheme
+            scheme_id=self.scheme,
         )
-
 
     def test_create_scheme(self):
         data = {
             "name": "Give all unemployed with household kids 1000",
-            "benefits":[{
-                
-                "amount": 1000,
-                "apply_household": True
-            }],
-            "criterias": [{
-                "name": "All unemployed",
-                "ops": "=",
-                "threshold": "unemployed",
-                "field": "employment_status"
-            },
-            {
-                "name": "Household kids",
-                "ops": "<=",
-                "threshold": "16",
-                "field": "age",
-                "apply_household": True
-            }
-            ]
+            "benefits": [{"amount": 1000, "apply_household": True}],
+            "criterias": [
+                {
+                    "name": "All unemployed",
+                    "ops": "=",
+                    "threshold": "unemployed",
+                    "field": "employment_status",
+                },
+                {
+                    "name": "Household kids",
+                    "ops": "<=",
+                    "threshold": "16",
+                    "field": "age",
+                    "apply_household": True,
+                },
+            ],
         }
-        response = self.client.post(self.base_url, data, follow=True, format='json')
+        response = self.client.post(self.base_url, data, follow=True, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_scheme(self):
         response = self.client.get(self.base_url, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['name'], "Give all unemployed with household kids 1000")
+        self.assertEqual(
+            response.data[0]["name"], "Give all unemployed with household kids 1000"
+        )
 
     def test_partial_update_scheme(self):
         data = {"name": "random"}
-        response = self.client.patch(f'{self.base_url}/{self.scheme.uid}', data, format='json')
+        response = self.client.patch(
+            f"{self.base_url}/{self.scheme.uid}", data, format="json"
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], data["name"])
+        self.assertEqual(response.data["name"], data["name"])
+
+    def test_eligible(self):
+        response = self.client.get(f"{self.base_url}/eligible?applicant=", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(
+            response.data[0]["name"], "Give all unemployed with household kids 1000"
+        )
